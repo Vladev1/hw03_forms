7ca94f95b5c1d1b6f4c3d861f73c9864cef4b4e3
Владимир Мытарев <Vlaev17@yandex.ru>
2022-03-21 13:59:33 +0300

    1 review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -73,7 +73,7 @@ def post_create(request):
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     user = request.user.username
-    if post.author != user:
+    if post.author == user:
         return redirect('posts:post_detail', post_id=post_id)
     form = PostForm(request.POST or None, instance=post)
     if not form.is_valid():
